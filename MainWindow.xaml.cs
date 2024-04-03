1670e836d97a51447a5cf4ba7758634e8b77e5fc
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 19:10:44 +0700

    Chansov0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,6 +7,27 @@ using System.Windows;
 namespace WpfApp1
 {
     
+    private void AddNumber_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(numberTextBox.Text))
+        {
+            if (int.TryParse(numberTextBox.Text, out int number))
+            {
+                numbers.Add(number);
+                UpdateListBox();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите допустимое целое число».");
+            }
+        }
+        else
+        {
+            MessageBox.Show("Пожалуйста, введите число.");
+        }
+    }
+
+    
         private void ClearList_Click(object sender, RoutedEventArgs e)
         {
             numbers.Clear();
