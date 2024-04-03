dfb1d9a8906a23785b99072742a066841df89808
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 19:07:00 +0700

    Functii

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,7 +6,59 @@ using System.Windows;
 
 namespace WpfApp1
 {
-    
+    public partial class MainWindow : Window
+    {
+
+        List<int> numbers = new List<int>();
+
+        private void AddNumber_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(numberTextBox.Text))
+            {
+                if (int.TryParse(numberTextBox.Text, out int number))
+                {
+                    numbers.Add(number);
+                    UpdateListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите допустимое целое число».");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите число.");
+            }
+        }
+
+        private void MultiplyByFactor_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(numberTextBox.Text))
+            {
+                if (int.TryParse(numberTextBox.Text, out int factor))
+                {
+                    numbers = numbers.Select(num => num * factor).ToList();
+                    UpdateListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите допустимый множитель.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите множитель.");
+            }
+        }
+
+        private void UpdateListBox()
+
+        {
+            numberListBox.ItemsSource = null;
+            numberListBox.ItemsSource = numbers;
+
+        }
+    }
 }
         
          
