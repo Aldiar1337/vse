277da54cad67bdbe44c76a099efd29de08e34f26
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 19:52:32 +0700

    Functia

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Windows;
 
 
-namespace WpfApp1
-{
-    public partial class MainWindow : Window
-    {
+
 
         List<int> numbers = new List<int>();
 
@@ -58,8 +55,7 @@ namespace WpfApp1
             numberListBox.ItemsSource = numbers;
 
         }
-    }
-}
-        
+    
+  
          
 
