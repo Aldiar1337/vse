ef676cecf90132e96ef5a768f3af71ebfdbc5ea1
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 19:52:07 +0700

    Functia

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 
+namespace WpfApp1
+{
 
 
     public partial class MainWindow : Window
     {
-    
 
-    private void SortAscending_Click(object sender, RoutedEventArgs e)
-    {
-        numbers.Sort();
-        UpdateListBox();
+
+        private void SortAscending_Click(object sender, RoutedEventArgs e)
+        {
+            numbers.Sort();
+            UpdateListBox();
+        }
+
+        // Отсортировать по убыванию
+        private void SortDescending_Click(object sender, RoutedEventArgs e)
+        {
+            numbers.Sort();
+            numbers.Reverse();
+            UpdateListBox();
+        }
+
     }
-
-    // Отсортировать по убыванию
-    private void SortDescending_Click(object sender, RoutedEventArgs e)
-    {
-        numbers.Sort();
-        numbers.Reverse();
-        UpdateListBox();
-    }
-
-
+}
