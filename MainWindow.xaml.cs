fffd542c8a997ddb48e1ccb6a0a95e4c3367734d
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 18:56:55 +0700

    Functia3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,22 @@
 ﻿using System;
- 
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+
+        private void ClearList_Click(object sender, RoutedEventArgs e)
+        {
+            numbers.Clear();
+            UpdateListBox();
+        }
+
+    }
+}
+        
+         
 
